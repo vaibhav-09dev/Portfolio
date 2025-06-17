6c05b796c37c87d0215ef0de96af8c8083cafb08
unknown <vaibhav722322@gmail.com>
2025-06-17 21:19:31 +0530

    6th

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -103,7 +103,7 @@ export function Contact() {
       await axios.get("https://vaibhavdev-eight.vercel.app/api/Sendmail");
 
     }catch (err) {
-      toast("Error adding user");
+      
       setuser({
         Name: "",
         email: "",
@@ -266,9 +266,10 @@ export function Contact() {
   />
   <button
     type="submit"
+    disabled={loading}
     className="w-full h-11 bg-white flex justify-center items-center text-black rounded-2xl font-semibold"
   >
-    Send Message <span className="ml-2 text-black font-bold"><FaRegPaperPlane size={24} /></span>
+    {loading ? "Sending..." : "Send Message"} <span className="ml-2 text-black font-bold"><FaRegPaperPlane size={24} /></span>
   </button>
 </form>
                   </div>
